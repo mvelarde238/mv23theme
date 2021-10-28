0247f11f7db12d8f80c345db107c0661c89d0f8a
mvelarde238 <velarde23soluciones@gmail.com>
2021-10-28 16:37:55 -0500

    post object correction

--- inc/shortcodes/posts.php
+++ inc/shortcodes/posts.php
@@ -1,13 +1,20 @@
 <?php
 function print_posts( $atts ){
     if( is_singular( 'archive_page' ) ) return '--posts--';
-    $queried_object = get_taxonomy(get_queried_object()->taxonomy)->object_type;
+
+    if(is_tax()){
+        $queried_object = get_taxonomy(get_queried_object()->taxonomy)->object_type;
+        $object_type = $queried_object[0];
+    } else {
+        $queried_object = get_queried_object();
+        $object_type = $queried_object->name;
+    }
 
     if (have_posts()) :
         ob_start();
         echo '<div class="posts-listing posts-listing--style1">';
         while (have_posts()) : the_post();    
-            get_template_part( 'inc/partials/minipost', $queried_object[0] );
+            get_template_part( 'inc/partials/minipost', $object_type );
         endwhile;
         echo '</div>';
         return ob_get_clean();
