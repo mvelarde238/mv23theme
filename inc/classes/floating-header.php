a0b8c845a5e5e34887567d222822a3759dc41707
mvelarde238 <velarde23soluciones@gmail.com>
2022-04-27 13:44:55 -0500

    (Floating Header Class) consider replace_logo meta

--- inc/classes/floating-header.php
+++ inc/classes/floating-header.php
@@ -16,6 +16,9 @@ class Floating_Header{
 	}
 
 	public function set_overrided(){
+		$replace_logo = get_metadata($this->page_type, $this->page_ID,'replace_logo', true);
+ 		if ($replace_logo) $this->floating_header_overrided = true;
+
  		$custom_fixed_header = get_metadata($this->page_type, $this->page_ID,'custom_fixed_header', true);
  		if ($custom_fixed_header) $this->fixed_header_overrided = true;
 
@@ -27,15 +30,21 @@ class Floating_Header{
 		$options = array();
 
 		if ($this->floating_header_overrided) {
- 			$logo_version = get_metadata($this->page_type, $this->page_ID,'floating_header_logo', true);
- 			$bgc = get_metadata($this->page_type, $this->page_ID,'floating_header_bgc', true);
- 			$options['floating_text_color'] = get_metadata($this->page_type, $this->page_ID,'floating_header_color_scheme', true);
+			$replace_logo = get_metadata($this->page_type, $this->page_ID,'replace_logo', true);
+			if ($replace_logo){
+				$logo_id = get_metadata($this->page_type, $this->page_ID,'header_logo', true);
+			} else {
+				$logo_version = get_metadata($this->page_type, $this->page_ID,'floating_header_logo', true);
+				$logo_id = get_option( $logo_version );
+			}
+			$bgc = get_metadata($this->page_type, $this->page_ID,'floating_header_bgc', true);
+			$options['floating_text_color'] = get_metadata($this->page_type, $this->page_ID,'floating_header_color_scheme', true);
  		} else {
 			$logo_version = get_option( 'floating_header_logo' );
  			$bgc = get_option( 'floating_header_bgc' );
 			$options['floating_text_color'] = get_option( 'floating_header_color_scheme' );
+			$logo_id = get_option( $logo_version );
  		}
- 		$logo_id = get_option( $logo_version );
  		$logo_url = ($logo_id) ? wp_get_attachment_image_src( $logo_id, 'full') : null; 
  		if (is_array($logo_url) && $logo_url[0]):
  			$options['logo'] = $logo_url[0];
