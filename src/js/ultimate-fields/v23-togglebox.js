40d58b206397e8289dcee41263b63369729c55d8
mvelarde238 <velarde23soluciones@gmail.com>
2022-08-25 18:59:46 -0500

    v23ToggleBox.js version '5.8.27'

--- src/js/ultimate-fields/v23-togglebox.js
+++ src/js/ultimate-fields/v23-togglebox.js
@@ -21,7 +21,7 @@
 	"use strict";
 
 	var instances = [],
-		version = '5.8.23',
+		version = '5.8.27',
 		timers = {};
 
 	/**
@@ -48,6 +48,7 @@
 			}
 		}
 
+		this.nav = this.el.getElementsByClassName('v23-togglebox__nav')[0];
 		this.itemsBox = this.el.getElementsByClassName('v23-togglebox__items')[0];
 
 		this.items = [];
@@ -67,13 +68,11 @@
 			var dataOptions = {},
 				dataHeaderHeight = this.el.dataset.headerheight,
 				dataDesktopTemplate = this.el.dataset.desktoptemplate,
-				dataMovilTemplate = this.el.dataset.moviltemplate,
-				dataOpenfirstTab = this.el.dataset.openfirsttab;
+				dataMovilTemplate = this.el.dataset.moviltemplate;
 
 			if (dataDesktopTemplate != undefined) dataOptions.desktopTemplate = dataDesktopTemplate; 
             if (dataMovilTemplate != undefined) dataOptions.movilTemplate = dataMovilTemplate;
             if (dataHeaderHeight != undefined) dataOptions.headerHeight = dataHeaderHeight;
-            if (dataOpenfirstTab != undefined) dataOptions.openFirstTab = dataOpenfirstTab;
 
             // dataOptions are overriddden if options arg is passed
 			this.options = options = _extend(dataOptions, options);
@@ -83,8 +82,7 @@
 				desktopTemplate : 'tab',
 				movilTemplate : 'accordion',
 				breakpoint : 768,
-				headerHeight : 0,
-				openFirstTab : true
+				headerHeight : 0
 			};
 	
 			// Set default options
@@ -102,8 +100,7 @@
 			return true;
 		},
 		_saveItems(){
-			var nav = this.el.getElementsByClassName('v23-togglebox__nav')[0];
-			var btns = nav.getElementsByClassName('v23-togglebox__btn');
+			var btns = this.el.getElementsByClassName('v23-togglebox__btn');
 			for (var i = 0; i < btns.length; i++) {
 				var boxid = btns[i].dataset.boxid;
 
@@ -117,18 +114,17 @@
 			}
 		},
 		_attach_click_events(){
-			for (var i = 0; i < this.items.length; i++) {
-				_on(this.items[i].btn,'click', this._open_tab);
-			}
+			_on(this.el, 'click', this._open_tab);
 		},
 		_open_tab(event){
 			event.preventDefault();
-			this._handle_active_class(event.target);
+			var item = _hasClass(event.target, 'v23-togglebox__btn') ? event.target : _findAncestor(event.target, '.v23-togglebox__btn');
+			if(item) this._handle_active_class(item);
 		},
 		_handle_active_class(btn){
 			if (btn) { // method is triggered by a user click event
 				for (var i = 0; i < this.items.length; i++) {
-					if ( this.items[i].btn === btn ) {
+					if ( this.items[i].btn.dataset.boxid === btn.dataset.boxid ) {
 						if (this.currentTemplate === 'accordion'){
 							_toggleClass(btn, 'active');
 							_toggleClass(this.items[i].box, 'active');
@@ -148,7 +144,7 @@
 					_removeClass(this.items[i].box, 'active');	
 				};				
 
-				if (this.currentTemplate === 'tab' && String(this.options.openFirstTab) === 'true') {
+				if (this.currentTemplate === 'tab') {
 					_addClass(this.items[0].btn, 'active');
 					_addClass(this.items[0].box, 'active');	
 				}
@@ -165,30 +161,17 @@
 		},
 		_handle_template(){
 			var options = this.options,
-				breakpoint = options.breakpoint;
-
+				breakpoint = options.breakpoint,
+				viewportWidth = _getViewportDimensions().width;
 			if( !(typeof breakpoint === 'number') ) return;
 
-      		if ( _getViewportDimensions().width <= breakpoint) {
-      			this.el.dataset.template = this.currentTemplate = options.movilTemplate;
-      		} else {
-      			this.el.dataset.template = this.currentTemplate = options.desktopTemplate;
-      		};
-			
-      		switch(this.currentTemplate){
-				case 'tab': 
-					for (var i = 0; i < this.items.length; i++) {
-						this.itemsBox.appendChild(this.items[i].box);
-					};
-					break;
-				case 'accordion':
-					for (var it = 0; it < this.items.length; it++) {
-						_insertAfter(this.items[it].box, this.items[it].btn);
-					};
-					break;
-				default:
-					break;
-      		}
+			var currentTemplate = (viewportWidth <= breakpoint) ? options.movilTemplate : options.desktopTemplate;
+      		this.el.dataset.template = this.currentTemplate = currentTemplate;
+
+			for (var i = 0; i < this.items.length; i++) {
+				if(this.currentTemplate=='tab') this.nav.appendChild(this.items[i].btn);
+				if(this.currentTemplate=='accordion') _insertBefore(this.items[i].btn, this.items[i].box);
+			};
 		},
 		_attach_resize_events(){
 			var timeToWaitForLast = 100, 
@@ -204,6 +187,77 @@
 					that._handle_active_class();
 				}, timeToWaitForLast, id);
 			}, true);
+		},
+		/**
+	 	* Add a New Item
+	 	* @param {Object}      [options]
+		* {
+		* id:           (required) (string)
+		* btn:          (optional) (obj) { content:`` }
+		* box:          (optional) (obj) { content:`` }
+		* setActive:    (optional) (bool)
+		* afterAddItem: (optional) (function)
+		* silent:       (optional) (bool) if true there is not cloning neither adding operations
+		*                                 useful when working with models that render the view themselves
+		*                                 btn: (required) (DOMElement)
+		*                                 box: (required) (DOMElement)
+		* }
+	 	*/
+		addItem(options){
+			if(!options.silent){
+				if(options.id === undefined) return;
+
+				var newBtn = this.items[0].btn.cloneNode(true);
+				newBtn.innerHTML = (options.btn && options.btn.content) ? options.btn.content : 'New Item';
+				newBtn.dataset.boxid = '#'+options.id;
+				
+				var newBox = this.items[0].box.cloneNode(true);
+				newBox.innerHTML = (options.box && options.box.content) ? options.box.content : 'Lorem ipsum dolor sit amet consectetur...';
+				newBox.id = options.id;
+				
+				if(this.currentTemplate == 'tab') this.nav.appendChild(newBtn);
+				if(this.currentTemplate == 'accordion') this.itemsBox.appendChild(newBtn);
+				this.itemsBox.appendChild(newBox);
+			} else {
+				var newBtn = options.btn;
+				var newBox = options.box;
+			}
+
+			this.items.push({ btn: newBtn, box: newBox });
+			if(options.setActive) this._handle_active_class(newBtn);
+			if(typeof options.afterAddItem == 'function') options.afterAddItem(newBtn,newBox);
+		},
+		/**
+	 	* Remove Item
+	 	* @param {Object}      [options]
+		* {
+		* index:           (required) (integer) index of item to be removed
+		* setActive:       (optional) (integer) index of item to be set as active
+		* afterRemoveItem: (optional) (function)
+		* silent:          (optional) (bool) if true there is not removing operations
+		*                                 useful when working with models that manage the view themselves
+		* }
+	 	*/
+		removeItem(options){
+			var index = options.index;
+			if(index <= this.items.length && this.items.length > 1 && index >= 0){
+				if(!options.silent){
+					this.items[index].btn.remove();
+					this.items[index].box.remove();
+				}
+				var deletedItem = this.items.splice(index, 1);
+				
+				if(options.setActive != undefined){
+					if(options.setActive >= 0 && options.setActive <= this.items.length){
+						this.items[options.setActive].btn.click();
+					}
+				} else {
+					if(this.currentTemplate == 'tab' && _hasClass(deletedItem[0].btn,'active')){
+						this.items[(this.items.length - 1)].btn.click();
+					}
+				}
+				if(typeof options.afterRemoveItem == 'function') options.afterRemoveItem(deletedItem);
+			}
 		}
 	};
 
@@ -277,6 +331,11 @@
     	animateScroll();
 	};
 
+	function _findAncestor(el, selector) {
+		while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,selector)));
+		return el;
+	}
+
 	Math.easeInOutQuad = function (t, b, c, d) {
 		//t = current time
 		//b = start value
@@ -304,6 +363,10 @@
     	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 	};
 
+	function _insertBefore(newNode, referenceNode) {
+    	referenceNode.parentNode.insertBefore(newNode, referenceNode);
+	};
+
 	function _waitForFinalEvent(callback, ms, uniqueId) {
 		if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
 			timers[uniqueId] = setTimeout(callback, ms);
@@ -328,11 +391,11 @@
 		console.log( version );
 	};
 
-	V23_ToggleBox.init = function () {
+	V23_ToggleBox.init = function (options) {
 		var toggleboxes = document.getElementsByClassName('v23-togglebox');
 
         for (var i = 0; i < toggleboxes.length; i++) {
-            V23_ToggleBox.create( toggleboxes[i] );
+            V23_ToggleBox.create( toggleboxes[i], options);
         }
 
         return instances;
