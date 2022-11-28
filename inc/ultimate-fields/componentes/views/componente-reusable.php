64498c09e5ca7dbd074040fc369fd63d9519a101
mvelarde238 <velarde23soluciones@gmail.com>
2022-11-28 12:39:26 -0500

    avoid section.page-module inside columns

--- inc/ultimate-fields/componentes/views/componente-reusable.php
+++ inc/ultimate-fields/componentes/views/componente-reusable.php
@@ -3,6 +3,11 @@ $modulos = get_post_meta( $componente['seccion_reusable'],'v23_modulos', true);
 
 if (is_array($modulos) && count($modulos) > 0) :
     foreach ($modulos as $modulo) :
-        print_module_view($modulo);
+        // print_module_view($modulo);
+        $componentes = $modulo['componentes'];
+        foreach ($componentes as $c) {
+			set_query_var('componente', $c);
+			get_template_part('/inc/ultimate-fields/componentes/views/' . $c['__type']);
+		}
     endforeach;
 endif;
