3dbf6dd3e3a74e9894075c753376ed575edf8550
mvelarde238 <velarde23soluciones@gmail.com>
2023-02-02 11:32:43 -0500

    fix scroll-animations to work with -correct- items

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11954,26 +11954,37 @@ function animateWidth(elem, start, end, duration, spanElem) {
           scrollAnimations = JSON.parse(elem.dataset.scrollAnimations);
         if (scrollAnimations.length > 0) {
           scrollAnimations.forEach(function (group) {
-            var triggerElement = group['trigger_element'] != 'this' ? group['trigger_element'] : elem;
-            var tweenElem = group['element'] != 'this' ? $(elem).find(group['element']) : elem;
-            var from = JSON.parse(group['from']);
-            var to = JSON.parse(group['to']);
-            var addIndicators = group['add_indicators'];
-
-            // var timeline = new TimelineMax();
-            // timeline.from(tweenElem, 1, from);
-            // timeline.to(tweenElem, 1, to);
-            var tween = TweenMax.fromTo(tweenElem, 1, from, to);
-            var scene = new ScrollMagic.Scene({
-              triggerElement: triggerElement,
-              duration: group['duration'],
-              triggerHook: group['trigger_hook'],
-              offset: group['offset']
-            }).setTween(tween).addTo(controller);
-            if (addIndicators == '1') scene.addIndicators();
+            var triggerElement = group['trigger_element'] != 'this' ? $(elem).find(group['trigger_element']) : elem;
+            if (triggerElement.length) {
+              for (var index = 0; index < triggerElement.length; index++) {
+                var _tgrEl = triggerElement[index];
+                var tweenElem = $(_tgrEl).find(group['element']);
+                addAnimation(_tgrEl, tweenElem, group);
+              }
+            } else {
+              var tweenElem = group['element'] != 'this' ? $(elem).find(group['element']) : elem;
+              addAnimation(triggerElement, tweenElem, group);
+            }
           });
         }
       }
+    }
+    function addAnimation(triggerElement, tweenElem, group) {
+      var from = JSON.parse(group['from']);
+      var to = JSON.parse(group['to']);
+      var addIndicators = group['add_indicators'];
+
+      // var timeline = new TimelineMax();
+      // timeline.from(tweenElem, 1, from);
+      // timeline.to(tweenElem, 1, to);
+      var tween = TweenMax.fromTo(tweenElem, 1, from, to);
+      var scene = new ScrollMagic.Scene({
+        triggerElement: triggerElement,
+        duration: group['duration'],
+        triggerHook: group['trigger_hook'],
+        offset: group['offset']
+      }).setTween(tween).addTo(controller);
+      if (addIndicators == '1') scene.addIndicators();
     }
   });
 })(jQuery, console.log);
