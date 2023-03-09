238c22578f79cfd2dc6166e4618d7983e3b172d4
mvelarde238 <velarde23soluciones@gmail.com>
2023-03-09 09:16:39 -0500

    Transform vimeo urls in iframes

--- inc/ultimate-fields/utils/oembed.php
+++ inc/ultimate-fields/utils/oembed.php
@@ -1,13 +1,19 @@
 <?php
 function oembed($post_content)
 {
-    $pattern = '@(http|https)://(www\.)?youtu[^\s]*@i';
     $matches = array();
-    preg_match_all($pattern, $post_content, $matches);
+    $paterns = array(
+        '@(http|https)://(www\.)?youtu[^\s]*@i',
+        '@(http|https)://(www\.)?vimeo[^\s]*@i'
+    );
 
-    foreach ($matches[0] as $match) {
-        $iframe = '<p><div class="video-responsive">'.wp_oembed_get($match).'</div></p>';
-        $post_content = str_replace($match, $iframe, $post_content);
+    foreach ($paterns as $patern) {
+        preg_match_all($patern, $post_content, $matches);
+        
+        foreach ($matches[0] as $match) {
+            $iframe = '<p><div class="video-responsive">'.wp_oembed_get($match).'</div></p>';
+            $post_content = str_replace($match, $iframe, $post_content);
+        }
     }
     return $post_content;
 };
