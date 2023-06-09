a0762f14be94c4e4d85856625df78e44e5fd4cc5
mvelarde238 <velarde23soluciones@gmail.com>
2023-06-09 13:00:08 -0500

    Just use the_content filter if is_singular() || is_page()

--- inc/ultimate-fields/utils/print-modules.php
+++ inc/ultimate-fields/utils/print-modules.php
@@ -120,9 +120,13 @@ function print_module_view($modulo)
 
 
 add_filter('the_content', function ($content) {
-	ob_start();
-	if ($content) echo '<div class="page-module"><div class="componente">' . $content . '</div></div>';
-	// I would liketo add ultimate_fields_page_content() here but dosnt work
-	$filtered_content = ob_get_clean();
-	return $filtered_content;
+	if( is_singular() || is_page() ){
+		ob_start();
+		if ($content) echo '<div class="page-module"><div class="componente">' . $content . '</div></div>';
+		// I would liketo add ultimate_fields_page_content() here but dosnt work
+		$filtered_content = ob_get_clean();
+		return $filtered_content;
+	} else {
+		return $content;
+	}
 }, 100);
