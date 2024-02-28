6d3e79e7b3399cd5d23aa3830840ce32e9bc7b14
mvelarde238 <velarde23soluciones@gmail.com>
2024-02-28 11:18:47 -0500

    CSS Implementation: columnas auto + fluid

--- inc/ultimate-fields/componentes/common-settings/row-settings.php
+++ inc/ultimate-fields/componentes/common-settings/row-settings.php
@@ -15,6 +15,7 @@ $row_settings = array(
             'columnas-1de3-2de3' => '1/3 + 2/3',
             'columnas-1de4-3de4' => '1/4 + 3/4',
             'columnas-3de4-1de4' => '3/4 + 1/4',
+            'columnas-auto-fluid' => 'auto + fluid',
         ))->add_dependency('nth_columnas','2','='),
     Field::create( 'select', 'tablet_widths','Ancho en Tablets')->set_width( 25 )->add_options( array(
             '' => '100%',
@@ -23,6 +24,7 @@ $row_settings = array(
             'tablet-1de3-2de3' => '1/3 + 2/3',
             'tablet-1de4-3de4' => '1/4 + 3/4',
             'tablet-3de4-1de4' => '3/4 + 1/4',
+            'tablet-auto-fluid' => 'auto + fluid',
         ))->add_dependency('nth_columnas','2','='),
     Field::create( 'select', 'mobile_widths','Ancho en Móviles')->set_width( 25 )->add_options( array(
             '' => '100%',
@@ -31,6 +33,7 @@ $row_settings = array(
             'mobile-1de3-2de3' => '1/3 + 2/3',
             'mobile-1de4-3de4' => '1/4 + 3/4',
             'mobile-3de4-1de4' => '3/4 + 1/4',
+            'mobile-auto-fluid' => 'auto + fluid',
         ))->add_dependency('nth_columnas','2','='),
     Field::create( 'select', 'special_widths_3', 'Ancho de las columnas')->set_width( 25 )->add_options( array(
             '' => '1/3 + 1/3 + 1/3',
