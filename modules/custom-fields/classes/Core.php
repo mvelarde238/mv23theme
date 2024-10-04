cf3ddb23a81ea1a31613361e493c526240fe32e3
mvelarde238 <velarde23soluciones@gmail.com>
2024-10-04 10:15:19 -0500

    Move accordion stuff (postmeta, hooks) to Theme workflow

--- modules/custom-fields/classes/Core.php
+++ modules/custom-fields/classes/Core.php
@@ -83,7 +83,6 @@ class Core{
         require_once( THEME_CUSTOM_FIELDS_DIR.'/containers/page-settings.php' );
         require_once( THEME_CUSTOM_FIELDS_DIR.'/containers/menu-item-data.php' );
         require_once( THEME_CUSTOM_FIELDS_DIR.'/containers/menu-item-megamenu.php' );
-        require_once( THEME_CUSTOM_FIELDS_DIR.'/containers/accordion-settings.php' );
         require_once( THEME_CUSTOM_FIELDS_DIR.'/containers/post-format.php' );
         require_once( THEME_CUSTOM_FIELDS_DIR.'/containers/featured-video.php' );
 
