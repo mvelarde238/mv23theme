93e40d7e896da35d9be41f022d92eb925f6ed278
mvelarde238 <velarde23soluciones@gmail.com>
2025-01-02 14:40:41 -0500

    Fix remove_empty_paragraphs() function

--- src/js/functions/remove-empty-paragraphs.js
+++ src/js/functions/remove-empty-paragraphs.js
@@ -1,8 +1,12 @@
 function remove_empty_paragraphs() {
+    // Selecciona todos los elementos <p> en el documento
     const paragraphs = document.querySelectorAll('p');
 
+    // Itera sobre cada elemento <p>
     paragraphs.forEach((p) => {
-        if (p.textContent.trim() === '') {
+        // Verifica si el <p> está vacío o contiene solo espacios
+        if (p.innerHTML.trim() === '' ) {
+            // Elimina el elemento <p> del DOM
             p.remove();
         }
     });
