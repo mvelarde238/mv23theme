cf49e8f750842ee6ca4856bb9f420f0b79b498a8
mvelarde238 <velarde23soluciones@gmail.com>
2025-02-25 19:22:22 -0500

    Hardening WP

--- Core/Admin/Hardening_WP.php
+++ Core/Admin/Hardening_WP.php
@@ -18,4 +18,18 @@ class Hardening_WP{
             add_filter('show_admin_bar', '__return_false');
         }
     }
+
+    public function redirect_author_page() {
+        if (is_author()) {
+            wp_redirect(home_url());
+            exit;
+        }
+    }
+
+    public function disable_rest_endpoints($endpoints) {
+        if (isset($endpoints['/wp/v2/users'])) {
+            unset($endpoints['/wp/v2/users']);
+        }
+        return $endpoints;
+    }
 }
