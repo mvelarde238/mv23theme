23c425e2f0854b727d3c94c8950239cdc5d51540
unknown <velarde23soluciones@gmail.com>
2026-04-16 20:32:54 -0500

    Implement Template_Engine::check_actions() on Icon and Text component

--- Core/Builder/Component/Icon_and_Text.php
+++ Core/Builder/Component/Icon_and_Text.php
@@ -98,6 +98,7 @@ class Icon_and_Text extends Component {
         $attributes = Template_Engine::generate_attributes( $args );
 		ob_start();
         do_action( 'after_component_wrapper_start', $args );
+        echo Template_Engine::check_actions('start', $args );
         echo '<div '.$attributes.'>';
         
         $icon_wrapper = $args['components'][0] ?? null;
@@ -110,8 +111,9 @@ class Icon_and_Text extends Component {
         $content_wrapper['additional_classes'][] = 'content-wrapper';
 		echo Template_Engine::getInstance()->handle( $content_wrapper );
 
-        echo '</div>';
         do_action( 'before_component_wrapper_end', $args );
+        echo '</div>';
+        echo Template_Engine::check_actions('end', $args );
 		return ob_get_clean();
 	}
 }
